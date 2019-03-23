227905c87fe998c18ebf9ef4e96affd2d304d851
yushi <yushihuang@gmail.com>
2019-03-24 03:52:44 +0800

    feat: support history

--- main.js
+++ main.js
@@ -23,6 +23,9 @@ function render (o, root) {
 function main () {
     var h = Heap.empty;
     var root = $('#heap');
+    var history = $('#history');
+    var current = -1;
+
     function rerender () {
         root.empty();
         var o = JSON.parse(Heap.json_of_heap(h));
@@ -38,10 +41,24 @@ function main () {
         h = Heap.insert(v, h);
         input.val(v+1);
         rerender();
+        pushHistory('insert '+v, h);
     });
 
     $('#deleteMin').click(function () {
         h = Heap.deleteMin(h);
+        rerender();
+        pushHistory('deleteMin', h);
+    });
+
+    function pushHistory (text, heap) {
+        history.children().slice(current+1).remove();
+        current += 1;
+        history.append($('<span>').text(text).data('heap', heap).data('index', current));
+    }
+
+    $('#history').on('click', 'span', function () {
+        h = $(this).data('heap');
+        current = $(this).data('index');
         rerender();
     });
 }
